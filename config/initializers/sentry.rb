198aaa1cd8085c188b761d200af4d37939beae2e
Alexander Tipugin <atipugin@gmail.com>
2023-04-19 15:27:53 +0600

    Don't sample `GET /up` reqs

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,5 +3,27 @@
 Sentry.init do |config|
   config.dsn = ENV.fetch('SENTRY_DSN')
   config.enabled_environments = %w[production]
-  config.traces_sample_rate = 0.5
+
+  # See: https://docs.sentry.io/platforms/ruby/guides/rails/configuration/sampling/
+  config.traces_sampler = lambda do |sampling_context|
+    next sampling_context[:parent_sampled] unless sampling_context[:parent_sampled].nil?
+
+    transaction_context = sampling_context[:transaction_context]
+    op = transaction_context[:op]
+    transaction_name = transaction_context[:name]
+
+    case op
+    when /http/
+      case transaction_name
+      when /up/
+        0.0
+      else
+        0.25
+      end
+    when /sidekiq/
+      0.25
+    else
+      0.0
+    end
+  end
 end
